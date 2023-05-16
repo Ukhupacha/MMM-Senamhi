a6ff186d11abf5a31af1cab102fe37b3f4844e5e
Ukhu <Ukhu>
2023-05-16 00:29:28 -0500

    senamhi module

---
+++ MMM-Senamhi.js
@@ -0,0 +1,57 @@
+Module.register("MMM-Senamhi", {
+    defaults: {
+        location_id: "0019"
+    },
+    // place to save the list of nodes sent back from helper
+    nodeList: {},
+    message: "MMM-Senamhi starting up",
+    notificationReceived(notification, payload){
+        Log.log("notification received=" + notification);
+        // if all modules are running
+        if(notification == 'ALL_MODULES_STARTED') {
+            //send our confic down to helper use
+            this.sendSocketNotification("CONFIG", this.config)
+            // get the playing content
+            this.message = "MMM-Senamhi waiting for content from api request"
+            this.sendSocketNotification("getcontent", null)
+        }
+    },
+    // helper sends back specific web page nodes scraped
+    socketNotificationReceived: function(notification, payload){
+        if (notification == 'node_date') {
+            Log.log("received content back from helper")
+            // save it
+            this.content = payload;
+            Log.log("there are " + this.content.length + " elements to display");
+            if (this.content.length == 0) {
+                this.message = "MMM-Senamhi No content found" + this.config.location;
+            }
+            // tell MM we have new stuff to display
+            // will cause getDom() to be called
+            this.updateDom(1)
+        }
+    },
+    getDom: function() {
+        // base wrapper for our content
+        wrapper = document.createElement("div");
+        if(this.content.length > 0 ) {
+            // loop the list of nodes (if any)
+            for (const [key, value] of Object.entries(this.content)) {
+                // create a hold for the text of this node
+                let w = document.createElement("div");
+                // get the node fron the returned object
+                let n = value['date'];
+                w.innerHTML = n;
+                // append this info to the base wrapper
+                wrapper.appendChild(w);
+            }
+        }
+        else {
+            wrapper.innerHTML = this.message;
+        }
+        // tell MM this our content to add to the MM dom
+        return wrapper;
+
+
+    }
+});
---
+++ node_helper.js
@@ -0,0 +1,97 @@
+var NodeHelper = require('node_helper');
+const axios = require('axios');
+const https = require('https');
+const { JSDOM } = require('jsdom');
+
+module.exports = NodeHelper.create ({
+    config: null,
+    debug: false,
+    init() {
+        console.log(`Init module helper: ${this.name}`);
+    },
+    start() {
+        console.log(`Starting module helper: ${this.name}`);
+    },
+    stop() {
+        console.log(`Stopping module helper: ${this.name}`);
+    },
+
+    // Handle messages from our module// each notificiation indicates a different messgage
+    // payload is a data structure that is different per message.. up to you to design this
+    socketNotificationReceived(notification, payload) {
+        console.log(this.name + " received a socket notification: " + notification + " - Payload " + payload);
+        // if config message from module
+        if (notification == "CONFIG") {
+            // save payload config info
+            this.config = payload
+        }
+        // module wants content from api
+        else if (notification == "getcontent") {
+            this.getcontent()
+
+        }
+    },
+    // get the selected dom nodes from the specific web site page
+    getcontent(){
+        //setting JSDOM parameters
+        this.getForecast(this.config.location_id)
+            .then(
+                // this is the no error return from 
+                (forecast) => {                    
+                    this.sendSocketNotification("node_data", forecast)
+                },
+                (error) => {
+                    console.log("error from getForecast =" + error);
+                }
+            )
+    },
+
+    // getForecast function
+    getForecast: async function(location_id) {
+
+        const locationUrl = this.getLocationUrl(location_id);
+        const { data: html } = await axios.get(locationUrl, {
+            headers: {
+                accept: '*/*',
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+            },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false
+            })
+        });
+        const dom = new JSDOM(html);
+        const $ = (selector) => dom.window.document.querySelector(selector);
+    
+        const title = $('.subtit-interior').textContent.trim();
+    
+        const forecastList = dom.window.document.querySelectorAll('.bg-gris');
+        const forecast = []
+    
+        const getDay = (day) => {
+            const date = day.querySelector('strong').textContent.trim();
+            const icon = day.querySelector('img').getAttribute('src');
+            const maxTemp = day.querySelector('.text-max').textContent;
+            const minTemp = day.querySelector('.text-min').textContent;
+            const desc = day.querySelector('.desc').textContent;
+            return {
+                date,
+                icon,
+                maxTemp,
+                minTemp,
+                desc
+            }    
+        }
+        forecastList.forEach((day) => {
+            forecast.push(getDay(day));
+        })
+
+        return forecast;
+    },
+
+    // function to get url base on location id
+    getLocationUrl: function(location_id) {
+        const locationUrl = (location_id) => `https://www.senamhi.gob.pe/?p=pronostico-detalle&localidad=${location_id}`;
+        return locationUrl;
+    }
+
+})
